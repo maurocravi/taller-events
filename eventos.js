52cf2cc62e4517b8ab18f440ed4dd374ff6b26e4
maurocravi <maurocravi@gmail.com>
2023-08-06 17:44:52 -0300

    agregados mensajes en boton y div

--- eventos.js
+++ eventos.js
@@ -1,9 +1,15 @@
-const divmessage = document.getElementsById("divMessage");
-
+const divmessage = document.getElementById("divMessage");
 function saludoDiv() {
   alert("Hola! Soy el div");
 }
-
 divmessage.addEventListener("click", () => {
   saludoDiv();
+});
+
+const buttonMessage = document.querySelector("button");
+function mensajeButton() {
+  alert("Hola!");
+}
+buttonMessage.addEventListener("click", () => {
+  mensajeButton();
 });
