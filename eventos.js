304552e64ebb397491750534a041542e148ec471
maurocravi <maurocravi@gmail.com>
2023-08-07 21:47:36 -0300

    ahora si, funcionan los mensajes del div y del button

--- eventos.js
+++ eventos.js
@@ -1,15 +1,16 @@
 const divmessage = document.getElementById("divMessage");
-function saludoDiv() {
-  alert("Hola! Soy el div");
-}
-divmessage.addEventListener("click", () => {
-  saludoDiv();
-});
-
 const buttonMessage = document.querySelector("button");
+
 function mensajeButton() {
   alert("Hola!");
 }
 buttonMessage.addEventListener("click", () => {
   mensajeButton();
+});
+
+function saludoDiv() {
+  alert("Hola! Soy el div");
+}
+divmessage.addEventListener("click", () => {
+  saludoDiv();
 });
